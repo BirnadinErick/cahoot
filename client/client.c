327abc7987f1ed1ea3405c5031fc21ef8345bdac
zzzaki10 <120649118+zzzaki10@users.noreply.github.com>
2025-01-15 00:24:44 +0100

    Update client.c

--- client/client.c
+++ client/client.c
@@ -4,101 +4,101 @@
 int main() {
 
 	char testSubject, Answer;
-	int PlayerScore=0;
+	int PlayerScore = 0;
 
 	printf("Which of the two quizes would you like to take?");
 	printf("\nFor a German quiz, please enter the character 'G'");
-	printf("\nFor an English quiz please enter the character 'E'");
+	printf("\nFor a Geography quiz please enter the character 'E'");
 	printf("\nPlease enter your desired character: ");
 	scanf_s("\n%c", &testSubject);
-	printf("\n*Every question has ONLY 1 correct answer, please choose ONLY 1 of the four options given to you.*\n");
+	printf("\n*Every question has ONLY 1 correct answer, please choose ONLY 1 of the options given to you.*\n");
 
 	if (testSubject == 'G' || testSubject == 'g')
 	{
-		printf("\nGerman Question 1");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nAnna und Paula wollten/wollte/musste/solltet ihren Urlaub planen.");
+		printf("\n1. wollten \n2. wollte \n3. musste \n4. solltet");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
 		if (Answer == '1') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
 		}
 		else if (Answer == '2' | Answer == '3' | Answer == '4')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
 		}
 		else
 		{
 			printf("Invalid Data, please try again.");
 		}
 
-		printf("\nGerman Question 2");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nWir haben draussen uebernachtet, obwohl/ da es geregnet hat.");
+		printf("\n1. obwohl 2. da");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
-		if (Answer == '3') {
+		if (Answer == '1') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
 		}
-		else if (Answer == '1' | Answer == '4' | Answer == '2')
+		else if (Answer == '2')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
 		}
 		else
 		{
 			printf("Invalid Data, please try again.");
 		}
 
-		printf("\nGerman Question 3");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
-		printf("\nYour Answer: ");
-		scanf_s("\n%c", &Answer);
-
-		if (Answer == '3') {
-			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
-		}
-		else if (Answer == '1' | Answer == '4' | Answer == '2')
-		{
-			printf("Your asnwer is incorrect");
-		}
-		else
-		{
-			printf("Invalid Data, please try again.");
-		}
-
-		printf("\nGerman Question 4");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nLouis aergert sich, obwohl / weil er den Insektenschutz vergessen hat. ");
+		printf("\n1. obwohl 2. weil");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
 		if (Answer == '2') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
 		}
-		else if (Answer == '1' | Answer == '3' | Answer == '4')
+		else if (Answer == '1')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
 		}
 		else
 		{
 			printf("Invalid Data, please try again.");
 		}
 
-		printf("\nGerman Question 5");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nJannis hat ein Feuer gemacht, obwohl / weil das im Gebirge verboten ist.");
+		printf("\n1. obwohl 2. weil");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
 		if (Answer == '1') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
 		}
-		else if (Answer == '2' | Answer == '3' | Answer == '4')
+		else if (Answer == '2')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
+		}
+		else
+		{
+			printf("Invalid Data, please try again.");
+		}
+
+		printf("\nIch darf/durfte/konnte/soll heute nicht zu spät nach Hause kommen.");
+		printf("\n1. darf \n2. durfte \n3. konnte \n4. soll");
+		printf("\nYour Answer: ");
+		scanf_s("\n%c", &Answer);
+
+		if (Answer == '2') {
+			printf("Your answer is correct");
+			PlayerScore += 1;
+		}
+		else if (Answer == '1' | Answer == '3' | Answer == '4')
+		{
+			printf("Your answer is incorrect");
 		}
 		else
 		{
@@ -107,90 +107,90 @@ int main() {
 	}
 	else if (testSubject == 'E' || testSubject == 'e')
 	{
-		printf("\nEnglish Question 1");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
-		printf("\nYour Answer: ");
-		scanf_s("\n%c", &Answer);
-
-		if (Answer == '4') {
-			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
-		}
-		else if (Answer == '2' | Answer == '3' | Answer == '1')
-		{
-			printf("Your asnwer is incorrect");
-		}
-		else
-		{
-			printf("Invalid Data, please try again.");
-		}
-
-		printf("\nEnglish Question 2");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
-		printf("\nYour Answer: ");
-		scanf_s("\n%c", &Answer);
-
-		if (Answer == '3') {
-			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
-		}
-		else if (Answer == '1' | Answer == '4' | Answer == '2')
-		{
-			printf("Your asnwer is incorrect");
-		}
-		else
-		{
-			printf("Invalid Data, please try again.");
-		}
-
-		printf("\nEnglish Question 3");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
-		printf("\nYour Answer: ");
-		scanf_s("\n%c", &Answer);
-
-		if (Answer == '4') {
-			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
-		}
-		else if (Answer == '1' | Answer == '3' | Answer == '2')
-		{
-			printf("Your asnwer is incorrect");
-		}
-		else
-		{
-			printf("Invalid Data, please try again.");
-		}
-
-		printf("\nEnglish Question 4");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nWhich African country is the largest by area?");
+		printf("\n1. Sudan \n2. Algeria \n3. Democratic Republic of Congo \n4. Libya");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
 		if (Answer == '2') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
 		}
 		else if (Answer == '1' | Answer == '3' | Answer == '4')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
 		}
 		else
 		{
 			printf("Invalid Data, please try again.");
 		}
 
-		printf("\nEnglish Question 5");
-		printf("\n1. Answer 1 2. Answer 2 3. Answer 3 4. Answer 4");
+		printf("\nWhich country is famous for its tulips and windmills?");
+		printf("\n1. Belgium \n2. Denmark \n3. Netherlands \n4. Germany");
+		printf("\nYour Answer: ");
+		scanf_s("\n%c", &Answer);
+
+		if (Answer == '3') {
+			printf("Your answer is correct");
+			PlayerScore += 1;
+		}
+		else if (Answer == '1' | Answer == '4' | Answer == '2')
+		{
+			printf("Your answer is incorrect");
+		}
+		else
+		{
+			printf("Invalid Data, please try again.");
+		}
+
+		printf("\nWhat is the capital of South Korea?");
+		printf("\n1. Seoul \n2. Beijing \n3. Soul \n4. Daejeon");
 		printf("\nYour Answer: ");
 		scanf_s("\n%c", &Answer);
 
 		if (Answer == '1') {
 			printf("Your answer is correct");
-			PlayerScore += PlayerScore;
+			PlayerScore += 1;
+		}
+		else if (Answer == '4' | Answer == '3' | Answer == '2')
+		{
+			printf("Your answer is incorrect");
+		}
+		else
+		{
+			printf("Invalid Data, please try again.");
+		}
+
+		printf("\nWhat is the capital of Canada?");
+		printf("\n1. Toronto \n2. Ottawa \n3. Munich \n4. Lahore");
+		printf("\nYour Answer: ");
+		scanf_s("\n%c", &Answer);
+
+		if (Answer == '2') {
+			printf("Your answer is correct");
+			PlayerScore += 1;
+		}
+		else if (Answer == '1' | Answer == '3' | Answer == '4')
+		{
+			printf("Your answer is incorrect");
+		}
+		else
+		{
+			printf("Invalid Data, please try again.");
+		}
+
+		printf("\nWhat country is not a part of Europe");
+		printf("\n1. Brazil \n2. Portugal \n3. France \n4. Hungary");
+		printf("\nYour Answer: ");
+		scanf_s("\n%c", &Answer);
+
+		if (Answer == '1') {
+			printf("Your answer is correct");
+			PlayerScore += 1;
 		}
 		else if (Answer == '2' | Answer == '3' | Answer == '4')
 		{
-			printf("Your asnwer is incorrect");
+			printf("Your answer is incorrect");
 		}
 		else
 		{
@@ -201,7 +201,7 @@ int main() {
 	{
 		printf("Invalid Data");
 	}
-	
-	printf("%i", PlayerScore);
-	return PlayerScore;
+
+	printf("\nYour final score is, %i/5", PlayerScore);
+	//return PlayerScore;
 }
