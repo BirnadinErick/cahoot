b715249c0c421d83eade30b2a16fe7f9a3d4798a
Birnadin Erick <45619033+BirnadinErick@users.noreply.github.com>
2025-01-21 09:06:07 +0100

    touchup 1

--- client/client_v2.c
+++ client/client_v2.c
@@ -1,235 +1,199 @@
+#include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
+#define MAXUSERNAME 50
 #define maxPLayers 100
 
+struct Player {
+    char* player_name;
+    uint8_t player_score;
+    Player* next_player;
+};
+typedef struct Player Player;
+
+void add_new_player(Player* HEAD, char* player_name, uint8_t player_score) {
+    Player* player = (Player*)malloc(sizeof(Player));
+    if (player == NULL) {
+        puts(
+            "OS denied to allocate new memory. Plz go discipline your OS");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy_s(player->player_name, MAXUSERNAME, playerName);
+    player->player_score = playerScore;
+    player->next_player = NULL;
+    if (players_list == NULL) {
+        players_list = player;
+        return;
+    }
+
+    while (HEAD != NULL) {
+        HEAD = HEAD->next_player;
+    }
+    HEAD->next_player = player;
+    return;
+}
+
+int checkAnswer(char answer, char ref_answer, uint8_t playerCurrScore) {
+    if (answer != ref_answer) {
+        printf("Wrong answer dummy");
+        return playerCurrScore;
+    }
+
+    printf("correct answer!");
+    return playerCurrScore++;
+}
+
+uint8_t geographyQuiz(uint8_t playerScore) {
+    char Answer = 'x';
+
+    printf("\nWhich African country is the largest by area?");
+    printf(
+        "\n1. Sudan \n2. Algeria \n3. Democratic Republic of Congo "
+        "\n4. Libya");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '2', playerScore);
+
+    printf("\nWhich country is famous for its tulips and windmills?");
+    printf("\n1. Belgium \n2. Denmark \n3. Netherlands \n4. Germany");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '3', playerScore);
+
+    printf("\nWhat is the capital of South Korea?");
+    printf("\n1. Seoul \n2. Beijing \n3. Soul \n4. Daejeon");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '1', playerScore);
+
+    printf("\nWhat is the capital of Canada?");
+    printf("\n1. Toronto \n2. Ottawa \n3. Munich \n4. Lahore");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '2', playerScore);
+
+    printf("\nWhat country is not a part of Europe");
+    printf("\n1. Brazil \n2. Portugal \n3. France \n4. Hungary");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '1', playerScore);
+
+    return playerScore;
+}
+
+uint8_t germanQuiz(uint8_t playerScore) {
+    char Answer = 'x';
+
+    printf(
+        "\nAnna und Paula wollten/wollte/musste/solltet ihren Urlaub "
+        "planen.");
+    printf("\n1. wollten \n2. wollte \n3. musste \n4. solltet");
+    printf("\nYour Answer: ");
+    scanf_s(" %c", &Answer);
+    playerScore = checkAnswer(Answer, '1', playerScore);
+
+    printf(
+        "\nWir haben draussen uebernachtet, obwohl/ da es geregnet "
+        "hat.");
+    printf("\n1. obwohl 2. da");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '1', playerScore);
+
+    printf(
+        "\nLouis aergert sich, obwohl / weil er den Insektenschutz "
+        "vergessen hat. ");
+    printf("\n1. obwohl 2. weil");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '2', playerScore);
+
+    printf(
+        "\nJannis hat ein Feuer gemacht, obwohl / weil das im Gebirge "
+        "verboten ist.");
+    printf("\n1. obwohl 2. weil");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '1', playerScore);
+
+    printf(
+        "\nIch darf/durfte/konnte/soll heute nicht zu spaet nach Hause "
+        "kommen.");
+    printf("\n1. darf \n2. durfte \n3. konnte \n4. soll");
+    printf("\nYour Answer: ");
+    scanf_s("\n%c", &Answer);
+    playerScore = checkAnswer(Answer, '2', playerScore);
+
+    return playerScore;
+}
+
+void print_scoreboard() {}
+
 int main() {
-	char testSubject, Answer;
-	int PlayerScore;
-	int Quizover = 0;
-	char playerName[50];
-	char playerNames[maxPLayers][50];
-	int playerScores[maxPLayers];
-	int playerCount = 0;
+    Player* players_list = NULL;
+    char testSubject, Answer;
+    uint8_t PlayerScore;
+    char Quizover = '1';
+    char playerName[MAXUSERNAME];
+    char playerNames[maxPLayers][MAXUSERNAME];
+    int playerScores[maxPLayers];
+    int playerCount = 0;
 
-	while (!Quizover) {
-		PlayerScore = 0;
+    while (Quizover == '1') {
+        uint8_t playerScore = 0;
 
-		
-		printf("\nPlease enter your name: ");
-		scanf_s("%s", playerName, (unsigned int)sizeof(playerName));
+        printf("\nPlease enter your name: ");
+        scanf_s("%s", playerName, MAXUSERNAME);
 
-		printf("Hello %s, which of the two quizzes would you like to take?", playerName);
-		printf("\nFor a German quiz, please enter the character 'G'");
-		printf("\nFor a Geography quiz please enter the character 'E'");
-		printf("\nPlease enter your desired character: ");
-		scanf_s(" %c", &testSubject);
+        printf("Hello %s, which of the two quizzes would you like to take?",
+               playerName);
+    quiz_start:
+        puts("For a German quiz, please enter the character 'G'");
+        puts("For a Geography quiz please enter the character 'E'");
+        puts("Please enter your desired character: ");
+        scanf_s(" %c", &testSubject);
 
-		printf("\n*Every question has ONLY 1 correct answer, please choose ONLY 1 of the options given to you.*\n");
+        puts(
+            "Every question has ONLY 1 correct answer, please choose ONLY 1 "
+            "of the options given to you.*");
 
-		if (testSubject == 'G' || testSubject == 'g') {
-			printf("\nAnna und Paula wollten/wollte/musste/solltet ihren Urlaub planen.");
-			printf("\n1. wollten \n2. wollte \n3. musste \n4. solltet");
-			printf("\nYour Answer: ");
-			scanf_s(" %c", &Answer);
+        if (testSubject == 'G' || testSubject == 'g') {
+            playerScore = germanQuiz(playerScore);
+        } else if (testSubject == 'E' || testSubject == 'e') {
+            playerScore = geographyQuiz(playerScore);
+        } else {
+            puts(" **You have entered invalid data**, reenter!.");
+            goto quiz_start;
+        }
 
-			if (Answer == '1') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '2' || Answer == '3' || Answer == '4')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
+/*
+        strcpy_s(playerNames[playerCount], MAXUSERNAME, playerName);
+        playerScores[playerCount] = playerScore;
+        playerCount++;
+*/
 
-			printf("\nWir haben draussen uebernachtet, obwohl/ da es geregnet hat.");
-			printf("\n1. obwohl 2. da");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
+        add_new_player(players_list, playerName, playerScore);
 
-			if (Answer == '1') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '2')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
+        print_scoreboard();
 
-			printf("\nLouis aergert sich, obwohl / weil er den Insektenschutz vergessen hat. ");
-			printf("\n1. obwohl 2. weil");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
+        printf("\nDo you want to end the quiz? (1 for Yes, 0 for No): ");
+        scanf_s("%d", &Quizover);
+    }
 
-			if (Answer == '2') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '1')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
+    printf("\nFinal Results:\n");
+    Player * HEAD = players_list;
+    while (HEAD != NULL) {
+        printf("Player: %s, Score: %d/5\n", HEAD->player_name, HEAD->player_score);
+        HEAD = HEAD->next_player;
+    }
+/*
+    for (int i = 0; i < playerCount; i++) {
+        printf("Player: %s, Score: %d/5\n", playerNames[i], playerScores[i]);
+    }
+*/
 
-			printf("\nJannis hat ein Feuer gemacht, obwohl / weil das im Gebirge verboten ist.");
-			printf("\n1. obwohl 2. weil");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '1') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '2')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-
-			printf("\nIch darf/durfte/konnte/soll heute nicht zu spaet nach Hause kommen.");
-			printf("\n1. darf \n2. durfte \n3. konnte \n4. soll");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '2') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '1' || Answer == '3' || Answer == '4')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-		}
-		else if (testSubject == 'E' || testSubject == 'e')
-		{
-			printf("\nWhich African country is the largest by area?");
-			printf("\n1. Sudan \n2. Algeria \n3. Democratic Republic of Congo \n4. Libya");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '2') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '1' || Answer == '3' || Answer == '4')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-
-			printf("\nWhich country is famous for its tulips and windmills?");
-			printf("\n1. Belgium \n2. Denmark \n3. Netherlands \n4. Germany");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '3') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '1' || Answer == '4' || Answer == '2')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-
-			printf("\nWhat is the capital of South Korea?");
-			printf("\n1. Seoul \n2. Beijing \n3. Soul \n4. Daejeon");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '1') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '4' || Answer == '3' || Answer == '2')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-
-			printf("\nWhat is the capital of Canada?");
-			printf("\n1. Toronto \n2. Ottawa \n3. Munich \n4. Lahore");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '2') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '1' || Answer == '3' || Answer == '4')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-
-			printf("\nWhat country is not a part of Europe");
-			printf("\n1. Brazil \n2. Portugal \n3. France \n4. Hungary");
-			printf("\nYour Answer: ");
-			scanf_s("\n%c", &Answer);
-
-			if (Answer == '1') {
-				printf("Your answer is correct");
-				PlayerScore += 1;
-			}
-			else if (Answer == '2' || Answer == '3' || Answer == '4')
-			{
-				printf("Your answer is incorrect");
-			}
-			else
-			{
-				printf("Invalid Data, please try again.");
-			}
-		}
-		else
-		{
-			printf(" **You have entered invalid data**");
-		}
-
-
-		strcpy_s(playerNames[playerCount], sizeof(playerNames[playerCount]), playerName);
-		playerScores[playerCount] = PlayerScore;
-		playerCount++;
-
-
-		printf("\nDo you want to end the quiz? (1 for Yes, 0 for No): ");
-		scanf_s("%d", &Quizover);
-	}
-
-
-	printf("\nFinal Results:\n");
-	for (int i = 0; i < playerCount; i++) {
-		printf("Player: %s, Score: %d/5\n", playerNames[i], playerScores[i]);
-	}
-
-	return 0;
+    return 0;
 }
